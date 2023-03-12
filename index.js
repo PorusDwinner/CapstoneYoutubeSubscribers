6a1aeb27ca98fe6a5be06b64cca2f993e30ca821
PorusDwinner <amit2811995@gmail.com>
2023-03-12 09:47:12 +0530

    process.env.port added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = require('./app');
 const mongoose = require('mongoose');
-const port = 3000;
+const port = process.env.port || 3000;
 
 // Parser JSON bodies
 mongoose.set('strictQuery' , false);
